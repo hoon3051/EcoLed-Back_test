a462ef7e39896b6f4bda82cb2099d35f6a40df86
hoon3051 <hoon_3051@korea.ac.kr>
2024-01-18 16:25:12 +0900

    [add] db connection

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/Eco-Led/EcoLed-Back_test/controllers"
 	"github.com/Eco-Led/EcoLed-Back_test/initializers"
-	"net/smtp"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,13 +15,6 @@ func init() {
 	initializers.ConnectToDB()
 	initializers.SyncDB()
 }
-
-func sendEmail() {
-	smtp.PlainAuth(
-		"",
-		"hoon30512329@gmail.com",
-		""
-	)
 
 func main() {
 	fmt.Println("EcoLed!!")
